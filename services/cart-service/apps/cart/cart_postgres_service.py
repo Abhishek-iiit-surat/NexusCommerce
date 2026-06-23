fb748749e104594c8b7f91415c39d5da70e99d9e
Abhishek <abhishekgkp567856@gmail.com>
2026-06-23 23:03:38 +0530

    Fix: Did transaction wrapping of cart flush

--- services/cart-service/apps/cart/cart_postgres_service.py
+++ services/cart-service/apps/cart/cart_postgres_service.py
@@ -1,6 +1,7 @@
 from .models import Cart, CartItem
 from .exceptions import CartNotFoundError, CartItemNotFoundError, InvalidQuantityError
 from .cart_redis_service import CartRedisService
+from django.db import transaction
 
 class CartPostgresService:
 
@@ -8,17 +9,24 @@ class CartPostgresService:
         self.cart_redis_service = CartRedisService()
 
     def flush_cart(self, user_id, items):
+        """
+        get the existing cart of the user if exists, else create 
+        a new cart and then clear the cart then flush new items
+        to the cart and unmark dirty on the redis cart
+        """
         cart, _ = Cart.objects.get_or_create(user_id=user_id)
-        CartItem.objects.filter(cart=cart).delete()
 
-        for item in items:
-            CartItem.objects.create(
-                cart=cart,
-                product_id=item["product_id"],
-                quantity=item["quantity"],
-            )
-
-        # remove the user's flushed cart from dirty cart redis set
-        self.cart_redis_service.clear_dirty(user_id)
+        with transaction.atomic():
+            CartItem.objects.filter(cart=cart).delete()
+            cart_items = [
+                CartItem(
+                    cart=cart,
+                    product_id=item["product_id"],
+                    quantity=item["quantity"]
+                )
+                for item in items
+            ]
+            CartItem.objects.bulk_create(cart_items)
+            self.cart_redis_service.clear_dirty(user_id)
 
         
